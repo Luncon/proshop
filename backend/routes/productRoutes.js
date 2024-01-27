3435909c18a49f2be7e2948144f47863ca8a5b48
Luncon <smitlucas2563@gmail.com>
2024-01-26 19:41:07 -0500

    Added new Error Handling and Found an Error with productRoutes, (Commented Out)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
-
+//import { getProducts, getProductsById } from '../controllers/productController.js';
 
 router.get('/', asyncHandler(async (req, res) => {
     const products = await  Product.find({});
@@ -18,6 +18,9 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
     res.status(404).json({message: 'Product not found'});
 }));
+
+//router.route("/").get(getProducts);
+//router.route('/:id').get(getProductsById);
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 //import products from "./data/products.js";
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 connectDB();
 
@@ -15,14 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
-
-// app.get('/api/products', (req, res) => {
-//     res.json(products);
-// });
-
-// app.get('/api/products/:id', (req, res)=> {
-//     const product = products.find((p) => p._id === req.params.id);
-//     res.json(product);
-// });
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
