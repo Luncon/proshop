49c2b0ca40e0398b738e5add6eff9791978f98b5
Luncon <smitlucas2563@gmail.com>
2024-01-26 19:01:15 -0500

    Fixed Postman Proxy Issues

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();    
 import connectDB from './config/db.js';
-import products from "./data/products.js";
-const port = 3000 //process.env.PORT || 5000;
-import proudctRoutes from './routes/productRoutes.js'
+//import products from "./data/products.js";
+const port = process.env.PORT || 5000;
+import productRoutes from './routes/productRoutes.js';
 
 connectDB();
 
@@ -14,13 +14,15 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res)=> {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// });
+
+// app.get('/api/products/:id', (req, res)=> {
+//     const product = products.find((p) => p._id === req.params.id);
+//     res.json(product);
+// });
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
