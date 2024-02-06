0cd5ba050653dd734ce086b7fa33fbba37460b13
Luncon <smitlucas2563@gmail.com>
2024-02-06 14:25:35 -0500

    Started Implementing Redux Store and A[I Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -19,7 +21,7 @@ import ProductScreen from './screens/ProductScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen/>} />0
+      <Route index={true} path="/" element={<HomeScreen/>} />
       <Route path="/product/:id" element={<ProductScreen/>} />
 
     </Route>
@@ -29,7 +31,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store = {store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
