3dc7c366b35e87d9a1492154e9ab310f268920ed
Luncon <smitlucas2563@gmail.com>
2024-02-06 16:47:24 -0500

    Finished Add To Cart Functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart") ? JSON.parse
 (localStorage.getItem("cart")) : {cartItems : []}
-
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-}
 
 const cartSlice = createSlice({
     name: "cart",
@@ -24,27 +21,16 @@ const cartSlice = createSlice({
             }
 
 
-            //Get Item Price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.
-            qty, 0))
-
-            //Get Shipping Price
-            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-            //Get Tax Price
-            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
-
-            //Get Total Price
-            state.totalPrice = (
-                Number(state.itemsPrice) +
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-            ).toFixed(2) 
-
-            localStorage.setItem('cart', JSON.stringify(state));
+            return updateCart(state)
         },
+        removeFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload)
+
+            return updateCart(state)
+        }
     },
 });
 
-export const{addToCart} = cartSlice.actions;
+export const{addToCart, removeFromCart} = cartSlice.actions;
 
 export default cartSlice.reducer
